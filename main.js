ddc929b925db961154e490429bead98e0eba56d3
Polyakova Maria <polyakovamy@yandex.ru>
2022-11-23 19:21:43 +0300

    Создание модального окна

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-$("#btn1").click(function () {
-  $("#block1").toggle("d-none");
-});
+$(document).ready(function ($) {
+  // Клик по ссылке "Закрыть".
+  $(".popup__close").click(function () {
+    $(this).parents(".popup").fadeOut();
+    return false;
+  });
 
-$("#btn2").click(function () {
-  $("#block2").toggle("d-none");
+  // Клик по фону, но не по окну.
+  $(".popup").click(function (e) {
+    if ($(e.target).closest(".popup__main").length == 0) {
+      $(this).fadeOut();
+    }
+  });
+
+  // Исчезновение блока в первом ряду
+  $("#btn1").click(function () {
+    $("#block1").toggle("d-none");
+  });
+
+  // Изменение положения двух блоков во втором ряду
+  $("#btn2").click(function () {
+    $("#block2_1").toggleClass("g-c-23");
+    $("#block2_2").toggleClass("g-c-12");
+
+    $("#block2_1").toggleClass("g-c-12");
+    $("#block2_2").toggleClass("g-c-23");
+  });
 });
